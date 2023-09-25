21a1cb7e2c9774bd70a02ceaad54099f27e5ce92
unknown <yeasin18801@gmail.com>
2023-09-25 11:10:14 +0600

    Backend Feature Product model MCR create

---
+++ back-end/app/Http/Controllers/FeatureAreaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FeatureArea;
+use Illuminate\Http\Request;
+
+class FeatureAreaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\FeatureArea  $featureArea
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FeatureArea $featureArea)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\FeatureArea  $featureArea
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FeatureArea $featureArea)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\FeatureArea  $featureArea
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FeatureArea $featureArea)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\FeatureArea  $featureArea
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FeatureArea $featureArea)
+    {
+        //
+    }
+}
---
+++ back-end/app/Models/FeatureArea.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FeatureArea extends Model
+{
+    use HasFactory;
+}
